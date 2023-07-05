95aa40d01443b3c49e76cba9c0f542069176ef7a
kyrie <thb@yunji2025.com>
2023-07-05 17:28:11 +0800

    feat: update site-icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/icon/kyrie.jpg',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
